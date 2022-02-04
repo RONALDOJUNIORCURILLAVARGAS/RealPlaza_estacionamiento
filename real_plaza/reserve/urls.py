fa8632a4b4afab301e845ff00b1a0ef232467696
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-02-04 15:43:53 -0500

    mysql

--- real_plaza/reserve/urls.py
+++ real_plaza/reserve/urls.py
@@ -1,7 +1,9 @@
 from os import name
+from xml.etree.ElementInclude import include
 from . import views
 from django.urls import path
 app_name='reserve'
 urlpatterns = [
-    path('',views.ReserveView.as_view(),name='page-reserve'),
+    path('',views.ReserveView.as_view(),name='page-reserve'),  
+    
 ]
--- real_plaza/reserve/views.py
+++ real_plaza/reserve/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 
+
+
+
 class ReserveView(generic.TemplateView):
     template_name='reservas.html'
 # Create your views here.
+
