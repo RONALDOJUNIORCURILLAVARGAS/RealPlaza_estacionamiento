23a4c707146d7a97f55bbf981529427a9f2767e9
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-03-10 23:49:29 -0500

    estacionamiento

---
+++ real_plaza/static/js/cambio_color.js
@@ -0,0 +1,31 @@
+//Funcion para ejecutarse siempre al cargar
+(()=>{
+    
+    let  caja_reservada=document.getElementById('caja1')
+    let caja_sugerida=document.getElementById('caja2')
+    let cajas=document.getElementsByClassName('estaciones')
+    for (let i = 0; i < cajas.length; i++) {
+        cajas[i].style.background='rgb(196, 196, 196)'        
+    }
+    caja_reservada?(caja_reservada.style.background='red'):(caja_reservada)
+    caja_sugerida?(caja_sugerida.style.background='green') :(caja_sugerida)
+    for (let i = 0; i < cajas.length; i++) {
+        console.log(cajas[i].style.background )      
+    }
+})();
+let contador_reservas=0
+let estilo_anterior=null;
+function cambio(id) {
+    const element=document.getElementById('caja'+id);
+    if(element.style.background=='yellow' && contador_reservas==1)
+    { 
+       element.style.background=estilo_anterior
+       --contador_reservas
+    }
+    else if( (element.style.background=='rgb(196, 196, 196)' || element.style.background=='green') &&contador_reservas==0){
+        estilo_anterior=element.style.background
+        element.style.background='yellow'
+       
+       ++contador_reservas
+    } 
+}
